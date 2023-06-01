80556d91a32a4f2bb0eb32e602bef2ea052f1334
railymm23456 <130125241+railymm23456@users.noreply.github.com>
2023-06-01 23:41:25 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+alert("Welcome to 'Photo Page' ");
+function Function() {
+    document.getElementById("demo").style.backgroundColor = "green";
+    document.getElementById("demo").innerHTML ="Submitted Successfully!";
+  }
+  
